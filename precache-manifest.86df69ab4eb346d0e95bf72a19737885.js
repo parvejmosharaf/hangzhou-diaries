2020c0240d1153a42893ab962673bf478b3b0e1e
Parvej Mosharaf <parvej.mosharaf@outlook.com>
2021-06-08 14:14:58 +0800

    Updates

---
+++ precache-manifest.86df69ab4eb346d0e95bf72a19737885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becde0251feba404e9b11abf12d43867",
+    "url": "/hangzhou-diaries/index.html"
+  },
+  {
+    "revision": "220dec7386047f5ec30f",
+    "url": "/hangzhou-diaries/static/css/main.ad027ea3.chunk.css"
+  },
+  {
+    "revision": "d3087d36cfc8045e9055",
+    "url": "/hangzhou-diaries/static/js/2.2fee7bce.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/hangzhou-diaries/static/js/2.2fee7bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220dec7386047f5ec30f",
+    "url": "/hangzhou-diaries/static/js/main.e8adf7b3.chunk.js"
+  },
+  {
+    "revision": "95d32af14cefe907b5ae",
+    "url": "/hangzhou-diaries/static/js/runtime-main.b4146312.js"
+  }
+]);
